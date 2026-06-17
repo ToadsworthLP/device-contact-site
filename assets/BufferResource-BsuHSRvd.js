611fee8ae7f61b383d48ca75f8a8f35258025e7d
Toastworth <fiery.toadsworth@gmail.com>
2026-06-17 21:11:16 +0200

    happy bappy

--- assets/BufferResource-BsuHSRvd.js
+++ assets/BufferResource-BsuHSRvd.js
@@ -1,4 +1,4 @@
-import{w as U,c as g,d as c,u as S,e as _,f as m,E as I,h as p}from"./index-rfuwc3Az.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,c as g,d as c,u as S,e as _,f as m,E as I,h as p}from"./index-Xg4-FBuD.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
